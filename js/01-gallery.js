520cffa123181fa6cf356a73de02410ce5181e07
IvanovArtemNA <IvanovArtemNA@gmail.com>
2022-11-01 00:42:11 +0200

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryItemsEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryItemsEl.addEventListener("click", onImageClick);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class ="gallery__container">
+        <a class="gallery__link" href="${original}">
+          <img loading="lazy" class="gallery__image" 
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"/>
+        </a>
+        </div>`;
+    })
+    .join(``);
+}
+
+function onImageClick(e) {
+  e.preventDefault(); //Останавливат скачивание (предотвращает действие браузера по умолчанию)
+  const instance = basicLightbox.create(
+    `<img width="auto" height="auto" src="${e.target.dataset.source}">`
+  );
+  instance.show();
+  galleryItemsEl.addEventListener("keydown", (e) => {
+    // событие keydown срабатывает при нажатии клавиши
+    if (e.key === "Escape") instance.close();
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryItemsEl.insertAdjacentHTML("beforeend", galleryMarkup);
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img loading="lazy" class="gallery__image" 
+       src="${preview}" 
+       alt="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+const galleryLighBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  //   overlayOpacity: 1,
+  //   maxZoom: 2,
+});
+
+galleryLighBox.show();
